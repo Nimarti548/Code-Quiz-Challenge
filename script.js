40d5b003c97e244eaec4367d10c3d5c4639142cc
Nicolas Martinez <Nimarti548@gmail.com>
2020-10-24 19:49:39 -0700

    restructured html and almost finished  javascript

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-// sets variables for use
-let choiceOne = document.querySelector("#answerOne");
-let choiceTwo = document.querySelector("#answerTwo");
-let choiceThree = document.querySelector("#answerThree");
-let choiceFour = document.querySelector("#answerFour");
-let question = document.querySelector("#question");
-let response = document.querySelector(".response");
-let Score = 0;
-let timer = 0;
-let currentQuestion = -1;
 
 // lists questions to be answered
 let questArr = [{ 
     question:"What does CSS stand for?",
-    choice1:"Complex Style Sheet",
-    choice2:"Cascading Style Sheet",
-    choice3:"Compact Style Sheet",
-    choice4:"Computer Screen Sheet",
+    choices:["Complex Style Sheet","Cascading Style Sheet","Compact Style Sheet","Computer Screen Sheet"],
     response:"Cascading Style Sheet",
 },
 { 
-    question:"Which of the following function of an array object adds one or more elements to the front of an array and pushes the rest of the items one slot back?",
-    choice1:"unshift()",
-    choice2:"sort( )",
-    choice3:"toString( )",
-    choice4:"join( )",
+    question:"Which of the following functions of an array object adds one or more elements to the front of an array and pushes the rest of the items one slot back?",
+    choices:["unshift()","sort( )","toString( )","join( )"],
     response:"unshift()",
 },
-
+{
+    question:"Which of the following functions deletes an item from an array?",
+    choices:["pop()", "delete()","remove()","banish()"],
+    response:"pop()",
+},
+{
+    question:"Which would you use to add an Event Listener in JQuery?",
+    choices:[".addEventListener()", ".on()",".addEL()",".add()"],
+    response:".on()",
+}
 ]
-// starts timer when start button clicked
+
+// sets variables for use
+let score = 0;
+let currentQuestion = -1;
+let timeLeft = 0;
+let timer;
+
+
+//starts the countdown timer once user clicks the 'start' button
 function start() {
 
-    timeLeft = 75;
+    timeLeft = 120;
     document.querySelector("#timeLeft").innerHTML = timeLeft;
     
     timer = setInterval(function() {
@@ -46,8 +47,106 @@ function start() {
     
     next();
     }
-    // stops timer and ends game
-    function endGame() {
-        clearInterval(timer);
-    }
 
+    
+    //stop the timer to end the game 
+    function endGame() {
+    clearInterval(timer);
+    
+    let quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+    <input type="text" id="name" placeholder="First name"> 
+    <button onclick="setScore()">Set score!</button>`;
+    
+    document.querySelector("#quizBody").innerHTML = quizContent;
+    }
+    
+    //store the scores on local storage
+    function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.querySelector('#name').value);
+    getScore();
+    }
+    
+    
+    function getScore() {
+    let quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+    
+    document.querySelector("#quizBody").innerHTML = quizContent;
+    }
+    
+    //clears the score name and value in the local storage if the user selects 'clear score'
+    function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+    
+    resetGame();
+    }
+    
+    //reset the game 
+    function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+    
+    document.querySelector("#timeLeft").innerHTML = timeLeft;
+    
+    let quizContent = `
+    <h1>
+        JavaScript Quiz!
+    </h1>
+    <h3>
+        Click to play!   
+    </h3>
+    <button onclick="start()">Start!</button>`;
+    
+    document.querySelector("#quizBody").innerHTML = quizContent;
+    }
+    
+    //deduct 15seconds from the timer if user chooses an incorrect answer
+    function incorrect() {
+    timeLeft -= 15; 
+    next();
+    }
+    
+    //increases the score by 20points if the user chooses the correct answer
+    function correct() {
+    score += 20;
+    next();
+    }
+    
+    //loops through the questions 
+    function next() {
+    currentQuestion++;
+    
+    if (currentQuestion > questArr.length - 1) {
+        endGame();
+        return;
+    }
+    
+    let quizContent = "<h2>" + questArr[currentQuestion] + "</h2>"
+    
+    for (let buttonLoop = 0; buttonLoop < questArr[currentQuestion].choices.length; buttonLoop++) {
+    let buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+    buttonCode = buttonCode.replace("[CHOICE]", questArr[currentQuestion].choices[buttonLoop]);
+    if (questArr[currentQuestion].choices[buttonLoop] == questArr[currentQuestion].response) {
+        buttonCode = buttonCode.replace("[ANS]", "correct()");
+    } else {
+        buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+    quizContent += buttonCode
+    }
+    
+    
+    document.querySelector("#quizBody").innerHTML = quizContent;
+    }
